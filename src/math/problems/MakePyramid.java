04a60ffd9a8609b2af213472f558c5f05c27fd19
jahmed04 <jisanx2020@gmail.com>
2020-11-26 01:45:10 -0500

    midterm so far

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,6 +1,7 @@
 package math.problems;
 
 public class MakePyramid {
+
     public static void main(String[] args) {
 
         /*   Implement a large Pyramid of stars in the screen with java.
@@ -15,5 +16,5 @@ public class MakePyramid {
         */
 
 
-        }
+    }
   }
